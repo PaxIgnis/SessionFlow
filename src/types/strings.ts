02d719bdd99a63d3cd032c7e2fb31813e912bb59
PaxIgnis <35787599+PaxIgnis@users.noreply.github.com>
2025-03-29 22:30:05 +0100

    Add strings dict for settings page. #16

---
+++ src/types/strings.ts
@@ -0,0 +1,7 @@
+export const STRINGS: Record<string, string> = {
+  settings_general: 'General',
+  settings_windows: 'Windows',
+  settings_tabs: 'Tabs',
+  settings_favicons: 'Favicons',
+  settings_storage: 'Storage',
+}
